c06c4d91bea887ba87bbc6ef1b61a00312897716
Alexandre LESUR <alex.lesur.2006@gmail.com>
2025-12-05 15:45:07 +0100

    v

--- Projet-POO-Mots-Glisses/Jeu.cs
+++ Projet-POO-Mots-Glisses/Jeu.cs
@@ -14,7 +14,24 @@ namespace Projet_POO_Mots_Glisses
         private Joueur joueur1;
         private Joueur joueur2;
 
+        private static int tempstour = 10;
+        private static int tempspartie = 120;
+
         // Constructeurs
+        public Jeu()
+        {
+            this.dictionnaire = null;
+            this.plateau = null;
+            this.joueur1 = null;
+            this.joueur2 = null;
+        }
+        public Jeu(Dictionnaire dictionnaire, Plateau plateau, Joueur joueur1, Joueur joueur2)
+        {
+            this.dictionnaire = dictionnaire;
+            this.plateau = plateau;
+            this.joueur1 = joueur1;
+            this.joueur2 = joueur2;
+        }
 
         // Propriétés
 
