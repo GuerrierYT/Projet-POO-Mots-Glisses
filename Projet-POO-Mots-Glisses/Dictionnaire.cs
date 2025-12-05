2ec95c2b5f23412a834e06381779cc7bd43042e2
Roissiope <loris.ligonniere@edu.devinci.fr>
2025-12-05 13:20:56 +0100

    a

--- Projet-POO-Mots-Glisses/Dictionnaire.cs
+++ Projet-POO-Mots-Glisses/Dictionnaire.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,46 @@ namespace Projet_POO_Mots_Glisses
 {
     internal class Dictionnaire
     {
-       a // Attributs
-            p,d
+        string nom;
+        private List<string> mots;
+
+        // Attributs
+        public Dictionnaire(string nom)
+        {
+            this.nom = nom;
+            mots = new List<string>();
+        }
+
+
+
+        public static List<string[]> LireMots()
+        {
+            string filename = "MotsFrancais.txt";
+            if (File.Exists(filename))      // On regarde si le fichier existe
+            {
+                string[] lines = File.ReadAllLines(filename);   // On lit toutes les lignes du fichier dans le tableau lines
+                List<string[]> tableauTrie = new List<string[]>();  // On crée une liste qui va regrouper les sous tableaux des mots triés
+                for (int i = 0; i<lines.Length; i+=2)
+                {
+                    string line = lines[i+1];
+                    string[] mots = line.Split(' ');
+                    tableauTrie.Add(mots);
+                }
+                return tableauTrie;
+            }
+            else
+            {
+                Console.WriteLine("Ce fichier n'existe pas.");
+                return null;
+            }
+        }
+
+    }
         // Constructeurs
 
         // Propriétés
 
         // Méthodes
-        q
+
     }
 }
