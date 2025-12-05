95b35d47698e842579765a1a12ae95fdeb3bdc55
Roissiope <loris.ligonniere@edu.devinci.fr>
2025-12-05 12:36:44 +0100

    yep

--- Projet-POO-Mots-Glisses/Program.cs
+++ Projet-POO-Mots-Glisses/Program.cs
@@ -11,6 +11,36 @@ namespace Projet_POO_Mots_Glisses
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenue dans le jeu des mots glissés !");
+            string mot = SaisirMot();
+            Console.WriteLine($"Le mot saisi est : {mot}");
+            Console.ReadLine();
         }
+
+        #region Saisie et validation du mot
+        static string SaisirMot()
+        {
+            string mot;
+            bool estValide = false;
+            do
+            {
+                Console.WriteLine("Veuillez saisir un mot d'au moins 2 lettres :");
+                mot = Console.ReadLine();
+                estValide = EstMotValide(mot);
+            }
+            while (mot.Length < 2 || estValide = false);
+        }
+        static bool EstMotValide(string mot)
+        {
+            foreach (char c in mot)
+            {
+                if (!char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        #endregion
     }
 }
