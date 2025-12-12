678b56da85617b73db3e7491690d7e899f74e54e
Alexandre LESUR <alex.lesur.2006@gmail.com>
2025-12-12 10:23:40 +0100

    v

--- Projet-POO-Mots-Glisses/Program.cs
+++ Projet-POO-Mots-Glisses/Program.cs
@@ -28,14 +28,14 @@ namespace Projet_POO_Mots_Glisses
             {
                 choix = QueFaire(espace, dico);
 
-                if (choix == 1)
+                if (choix == 1) // Début du jeu
                 {
                     Lettre lettre = new Lettre();
                     Jeu jeu = new Jeu();
 
                     #region création personnage
                     Console.WriteLine(ligne + "\n");
-                    Console.WriteLine(espace + "Creation des personnages...\n" + espace);
+                    Console.WriteLine(espace + "Creation des personnages :\n" + espace);
                     Console.WriteLine("Nommez le joueur 1 :");
                     string nomJ1 = Console.ReadLine();
                     string nomJ2 = nomJ1;
@@ -91,22 +91,6 @@ namespace Projet_POO_Mots_Glisses
 
         #endregion
 
-        #region Création du plateau
-        //Lettre lettre = new Lettre();
-        //Plateau plateau = new Plateau(lettre);
-        //Plateau plateau = new Plateau("plateautest.csv");
-        //Console.WriteLine(plateau);
-        //plateau.RechercheMot("BONJOUR");
-        //plateau.WriteFile("plateautest.csv");
-        #endregion
-        /*
-        Console.WriteLine("Bienvenue dans le jeu des mots glissés !");
-        string mot = SaisirMot();
-        Console.WriteLine($"Le mot saisi est : {mot}");
-        Console.WriteLine(dico.RechDichoRecursif(mot));
-        Console.ReadKey();
-        */
-
         #endregion
 
         #endregion
@@ -150,7 +134,7 @@ namespace Projet_POO_Mots_Glisses
             {
 
                 case 1: // Démarrer le jeu
-                    Console.WriteLine("Démarrage du jeu...");
+                    Console.WriteLine("Démarrage du jeu :");
                     break;
                 case 2: // Afficher les caractéristiques du dictionnaire
                     Console.WriteLine(dico.toString());
