676d684686a4ccb4e3e946a1eb6c6f19446118d8
Alexandre LESUR <alex.lesur.2006@gmail.com>
2025-12-15 09:03:45 +0100

    score

--- Projet-POO-Mots-Glisses/Joueur.cs
+++ Projet-POO-Mots-Glisses/Joueur.cs
@@ -49,8 +49,8 @@ namespace Projet_POO_Mots_Glisses
             {
                 int index = mot[i].CompareTo('A');
                 score += poids[index];
-                score += mot.Length * 2;
             }
+            score += mot.Length * 2;
             scores.Add(score);
             return score;
         }
